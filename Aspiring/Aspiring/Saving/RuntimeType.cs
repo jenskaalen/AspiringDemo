49ca2164cb2c7360695279202dc1e4772f564b8c
unknown <jenkal@Jensness.jeons.int>
2013-12-04 07:33:39 +0100

    Last commit before attempting to do a branchout for implementing serialization.
    The attempt is to use binary serialization to save the entire game.
    I did not think this far ahead. :<

---
+++ Aspiring/Aspiring/Saving/RuntimeType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspiringDemo.Saving
+{
+    public class RuntimeType
+    {
+        public Type RunType { get; set; }
+
+        public RuntimeType(Type type)
+        {
+            RunType = type;
+        }
+    }
+}
